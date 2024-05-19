88759693c4068fa0e80adb8d90141aa730be374b
Milan Thummar <thummar.fb@gmail.com>
2024-05-19 16:42:26 +0200

    create timelines - indicate current slider position

--- src/components/SliderButtons/SliderButtons.vue
+++ src/components/SliderButtons/SliderButtons.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="slider__btn-container">
-    <div class="slider__timelines"></div>
+    <div class="slider__timelines">
+      <div
+        v-for="(_, index) in products"
+        :key="index"
+        class="slider__timeline"
+        :style="{ width: timelineWidth + '%' }"
+      >
+        <div
+          class="slider__timeline-fill"
+          :style="{
+            transitionDuration: index === currentIndex ? '3s' : '0s',
+            width: index === currentIndex ? '100%' : '0',
+          }"
+        ></div>
+      </div>
+    </div>
     <div class="slide__prev-next">
       <button class="prev" @click="handlePrevButtonClick">&#10094;</button>
       <button class="next" @click="handleNextButtonClick">&#10095;</button>
@@ -10,7 +25,14 @@
 
 <script lang="ts">
 import "./SliderButtons.scss";
-import { defineComponent, watch, watchEffect } from "vue";
+import {
+  defineComponent,
+  ref,
+  watch,
+  onMounted,
+  onUnmounted,
+  watchEffect,
+} from "vue";
 
 export default defineComponent({
   name: "SliderButtons",
@@ -25,9 +47,46 @@ export default defineComponent({
     },
   },
   setup(props, { emit }) {
+    const timelineWidth = ref(0);
+
+    const calculateLineWidth = () => {
+      const slidesCount = props.products.length;
+      let width = 100 / slidesCount;
+      if (window.innerWidth >= 480 && width > 20) {
+        width = 18;
+      }
+      timelineWidth.value = width;
+    };
+
+    const updateLineWidth = () => {
+      const timeLines =
+        document.querySelectorAll<HTMLElement>(".slider__timeline");
+      timeLines.forEach((timeLine) => {
+        timeLine.style.width = `${timelineWidth.value}%`;
+      });
+    };
+
+    const handleResize = () => {
+      calculateLineWidth();
+      updateLineWidth();
+    };
+
+    onMounted(() => {
+      calculateLineWidth();
+      updateLineWidth();
+      window.addEventListener("resize", handleResize);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("resize", handleResize);
+    });
+
     watch(
       () => props.products.length,
-      () => {}
+      () => {
+        calculateLineWidth();
+        updateLineWidth();
+      }
     );
 
     const HandleBtnClick = (direction: "next" | "prev") => {
@@ -51,6 +110,7 @@ export default defineComponent({
     });
 
     return {
+      timelineWidth,
       handlePrevButtonClick,
       handleNextButtonClick,
     };
